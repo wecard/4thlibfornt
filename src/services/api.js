c8cd935535834c615bbaab86396069a86c24d3d5
Welisson Dias <welissondias@gmail.com>
2020-11-11 03:28:26 -0300

    ajuste

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: process.env.REACT_APP_API_URL
-    baseURL: 'https://fourlibback.herokuapp.com'
-});
-
+    URL: process.env.REACT_APP_API_URL,
+})
 
 export default api;

7538429f47c82594b5e8ca092982629f1cb7aa32
Welisson Dias <welissondias@gmail.com>
2020-11-11 22:07:41 -0300

    '.'

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    timeout:1000,
-    headers: {'X-Custom-Header':'foobar'}
 })
 
 export default api;

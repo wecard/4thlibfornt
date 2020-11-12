5a1e2b196626afefcf13c3ec123ca31ccdb73c0e
Welisson Dias <welissondias@gmail.com>
2020-11-11 21:26:42 -0300

    '.'

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-console.log(process.env.REACT_APP_API_URL);
+
 const api = axios.create({
-    url: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL,
 })
 
 export default api;
